ad99cd599e8ae6b2bd8d82224d8ccb501e80f166
Bicko <mbickerton@gmail.com>
2016-10-10 23:00:06 +0100

    Added checking whether vcard includes a name

--- vcfini.py
+++ vcfini.py
@@ -10,27 +10,36 @@ def main():
     vcf = open('00001.vcf', 'rU')
     record = []
 
-    #loop over the file, put individual vcards into a list
+    #Loop over the file, put individual vcards into separate
+    #     lists within the master list, record[]
     contact = -1
     for line in vcf:
-        print(line)
+        #print(line)
         if line.startswith('BEGIN:VCARD'):
             contact += 1
             record.append([])
         if line.startswith('FN:'):
-            record[contact].append(line[3:].strip())
+            #record[contact].append(line[3:].strip())
+            record[contact].append(line)
         if line.startswith('EMAIL'):
-            record[contact].append(line.strip())
+            record[contact].append(line)
         if line.startswith('TEL;'):
-            record[contact].append(line.strip())
-            
+            record[contact].append(line)
     vcf.close()
     
-    for i in record:
-        print('Record', i,)
-        input() #wait for enter
+    #Now format the lists correctly:
+    #   Name, telephone, email address (only one of each!)
+    names = 0
+    for person in record:
+        print('Record', person)
+        if any(entry.startswith('FN:') for entry in person):
+            print('Has a name')
+            names += 1
+        else:
+            print('No name!')
+        #input() #wait for enter
     print('\n', contact + 1, 'Contacts.')
-
+    print(names, 'have names.')
 
 if __name__ == '__main__':
   main()
